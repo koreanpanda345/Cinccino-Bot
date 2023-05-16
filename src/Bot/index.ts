79221eea6f13768e1d3e3f9f5c846efb17e10ead
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2023-05-16 16:58:43 -0700

    I Swear I will send you to jesus

--- src/Bot/index.ts
+++ src/Bot/index.ts
@@ -2,5 +2,6 @@ import { config } from "dotenv";
 import CinccinoBot from "./CinccinoBot";
 config();
 (async () => {
+	console.log("Successfully Started");
 	await CinccinoBot.startBot();
 })();
--- src/Bot/utils/loader.ts
+++ src/Bot/utils/loader.ts
@@ -4,12 +4,12 @@ export async function loadFolder(dir: string) {
 	let status = process.env.STATUS as string;
 	console.log(status);
 	try {
-		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `./app/src/Bot/modules/${dir}/**/*.js`;
+		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `./src/Bot/modules/${dir}/**/*.js`;
 		let files: string[] = sync(path);
 		console.log(files);
 		for(let file of files) {
 			await import(
-				file.replace(status === 'DEVELOPMENT' ? `src\\Bot` : `app\\src\\Bot`, '../')
+				file.replace(status === 'DEVELOPMENT' ? `src\\Bot` : `\\src\\Bot`, '../')
 			)
 			console.log(`File ${file} has been loaded`);
 		}
