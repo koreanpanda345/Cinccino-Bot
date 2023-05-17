f13df77b61668e280c6c49cba6425f9ef5625bec
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2023-05-16 17:04:20 -0700

    Will this fix it?

--- src/Bot/utils/loader.ts
+++ src/Bot/utils/loader.ts
@@ -4,7 +4,7 @@ export async function loadFolder(dir: string) {
 	let status = process.env.STATUS as string;
 	console.log(status);
 	try {
-		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `./src/Bot/modules/${dir}/**/*.js`;
+		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `./app/src/Bot/modules/${dir}/**/*.js`;
 		let files: string[] = sync(path);
 		console.log(files);
 		for(let file of files) {
