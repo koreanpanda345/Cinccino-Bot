64a5c12da728f08d13926f6d7c70b559de1c4ecd
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2023-05-16 17:12:26 -0700

    Found the problem, it was because it was looking for js files not ts files. EMOTIONAL DAMAGE

--- src/Bot/utils/loader.ts
+++ src/Bot/utils/loader.ts
@@ -7,12 +7,12 @@ export async function loadFolder(dir: string) {
 	console.log(path.resolve())
 	console.log(path.relative("./app/src/Bot/utils/loader.ts", "./app/src/Bot/modules/events/ready.ts"));
 	try {
-		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `./app/src/Bot/modules/${dir}/**/*.ts`;
+		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `/src/Bot/modules/${dir}/**/*.ts`;
 		let files: string[] = sync(path);
 		console.log(files);
 		for(let file of files) {
 			await import(
-				file.replace(status === 'DEVELOPMENT' ? `src\\Bot` : `app\\src\\Bot`, status === 'DEVELOPMENT' ? '../' : '../../')
+				file.replace(status === 'DEVELOPMENT' ? `src\\Bot` : `app\\src\\Bot`, '../')
 			)
 			console.log(`File ${file} has been loaded`);
 		}
