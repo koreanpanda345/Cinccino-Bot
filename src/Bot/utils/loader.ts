222837cdb3ce6dd89c0bd64db084e5a623658e5a
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2023-05-16 17:15:31 -0700

    Hopefully this will work

--- src/Bot/utils/loader.ts
+++ src/Bot/utils/loader.ts
@@ -1,18 +1,16 @@
 import { sync } from "glob";
-import path = require("path");
 
 export async function loadFolder(dir: string) {
 	let status = process.env.STATUS as string;
 	console.log(status);
-	console.log(path.resolve())
-	console.log(path.relative("./app/src/Bot/utils/loader.ts", "./app/src/Bot/modules/events/ready.ts"));
 	try {
-		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `/src/Bot/modules/${dir}/**/*.ts`;
+		let path = status === 'DEVELOPMENT' ? `./src/Bot/modules/${dir}/**/*.ts` : `./src/Bot/modules/${dir}/**/*.ts`;
+		console.log(path);
 		let files: string[] = sync(path);
 		console.log(files);
 		for(let file of files) {
 			await import(
-				file.replace(status === 'DEVELOPMENT' ? `src\\Bot` : `app\\src\\Bot`, '../')
+				file.replace(status === 'DEVELOPMENT' ? `src\\Bot` : `/src/Bot`, '../')
 			)
 			console.log(`File ${file} has been loaded`);
 		}
