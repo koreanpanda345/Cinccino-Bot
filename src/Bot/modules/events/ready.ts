768f3ab4a7559e865271387c9d2b41fde4047a3a
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2023-05-16 17:25:02 -0700

    Adding a status to the bot

--- src/Bot/modules/events/ready.ts
+++ src/Bot/modules/events/ready.ts
@@ -1,3 +1,5 @@
+import { ActivityType } from 'discord.js';
+import { client } from '../../core/client';
 import { createEvent } from '../../utils/creator';
 
 createEvent({
@@ -5,6 +7,12 @@ createEvent({
   id: 'ready',
   invoke: async () => {
     console.log('Client is ready');
+
+    client.user?.setActivity({
+      name: 'how cute I am!!',
+      type: ActivityType.Watching,
+    });
+
     await import('./../../utils/deployer');
   },
 });
