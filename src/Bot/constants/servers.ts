2fe4ccf312efbb4b2ddab710d2b1ab533a73bd5d
Cody Spratford <36426888+koreanpanda345@users.noreply.github.com>
2023-05-18 16:04:27 -0700

    Added support for radicalred.net

--- src/Bot/constants/servers.ts
+++ src/Bot/constants/servers.ts
@@ -1,23 +1,29 @@
 export const showdownServers: {
-	[key: string]: ShowdownServerType;
+  [key: string]: ShowdownServerType;
 } = {
-	showdown: {
-		name: "Showdown",
-		link: "https://play.pokemonshowdown.com",
-		ip: "sim3.psim.us:8000",
-		server: "ws://sim3.psim.us:8000/showdown/websocket",
-	},
-	dl: {
-		name: "DL",
-		link: "https://dl.psim.us",
-		ip: "dlsim.radicalred.net:8000",
-		server: "ws://dlsim.radicalred.net:8000/showdown/websocket"
-	}
+  showdown: {
+    name: 'Showdown',
+    link: 'https://play.pokemonshowdown.com',
+    ip: 'sim3.psim.us:8000',
+    server: 'ws://sim3.psim.us:8000/showdown/websocket',
+  },
+  dl: {
+    name: 'DL',
+    link: 'https://dl.psim.us',
+    ip: 'dlsim.radicalred.net:8000',
+    server: 'ws://dlsim.radicalred.net:8000/showdown/websocket',
+  },
+  radicalred: {
+    name: 'RadicalRed',
+    link: 'radicalred.net',
+    ip: 'sim.radicalred.net:8000',
+    server: 'ws://sim.radicalred.net:8000/showdown/websocket',
+  },
 };
 
 type ShowdownServerType = {
-	name: string;
-	link: string;
-	ip: string;
-	server: string;
-}
+  name: string;
+  link: string;
+  ip: string;
+  server: string;
+};
